cb7e83b89026f2279a168bbe9cb747381bfdfabd
clickclickcode <thanatosanyone@gmail.com>
2023-02-28 21:25:12 +0100

    Summary (required)

--- script/overlay.js
+++ script/overlay.js
@@ -1,20 +1,20 @@
-const openMaaltidskasse = document.querySelector("[data-maaltid]")
-const openOpskrifter = document.querySelector("[data-btn-opskrift]")
+// const openMaaltidskasse = document.querySelector("[data-maaltid]")
+// const openOpskrifter = document.querySelector("[data-btn-opskrift]")
 
 
-openMaaltidskasse.addEventListener('click', () => {
-    document.querySelector("[data-overlay-maaltidskasse]").style.display = 'block'
-    console.log('Knappen virker')
-})
+// openMaaltidskasse.addEventListener('click', () => {
+//     document.querySelector("[data-overlay-maaltidskasse]").style.display = 'block'
+//     console.log('Knappen virker')
+// })
 
 // closeMaaltidskasse.addEventListener('click', () => {
 //     document.querySelector("[data-]").style.display = 'none'
 // })
 
-openOpskrifter.addEventListener('click', () => {
-    document.querySelector("[data-overlay-opskrifter]").style.display = 'block'
-    console.log('Knappen virker')
-})
+// openOpskrifter.addEventListener('click', () => {
+//     document.querySelector("[data-overlay-opskrifter]").style.display = 'block'
+//     console.log('Knappen virker')
+// })
 
 // closeOpskrift.addEventListener('click', () => {
 //     document.querySelector("[data-]").style.display = 'none'
