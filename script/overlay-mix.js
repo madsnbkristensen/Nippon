6d728ec943f58734b0e0a11bc56c543b4d88aa2e
Emma2947 <emmakulriksen@gmail.com>
2023-03-02 21:52:39 +0100

    overlay - mixkassen

---
+++ script/overlay-mix.js
@@ -0,0 +1,10 @@
+var mixKasse = document.getElementById("overlay");
+
+function aabenKasse() {
+    mixKasse.style.display = "block";
+    console.log("test");
+  }
+  
+  function lukKasse() {
+    mixKasse.style.display = "none";
+  }
---
+++ script/overlay-ramen.js
@@ -0,0 +1,10 @@
+var ramenOpskrift = document.getElementById("overlayramen");
+
+function aabenOpskrift() {
+    ramenOpskrift.style.display = "block";
+    console.log("test");
+  }
+  
+  function lukOpskrift() {
+    ramenOpskrift.style.display = "none";
+  }
