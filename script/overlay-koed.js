d41883f119a907c4852707e89ba6eb05107d8737
Emma2947 <emmakulriksen@gmail.com>
2023-03-02 22:10:43 +0100

    .

---
+++ script/overlay-koed.js
@@ -0,0 +1,10 @@
+var koedKasse = document.getElementById("overlay2");
+
+function aabenKasse2() {
+    koedKasse.style.display = "block";
+    console.log("test");
+  }
+  
+  function lukKasse2() {
+    koedKasse.style.display = "none";
+  }
